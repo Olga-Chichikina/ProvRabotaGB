eef3c280509bb667d5ba915a84ef69f4ffb68234
Olga-6 <olga.chichikin1968@mail.ru>
2022-10-04 20:35:50 +0300

    Third commit

--- kont1/Program.cs
+++ kont1/Program.cs
@@ -3,6 +3,8 @@
 // Первоначальный массив можно ввести с клавиатуры, либо  задать на старте выполнения алгоритма . 
 
 string[] array= { "hello" , "12365" , "cat" , "dog" , " 5689" , "56"}; 
+Console.WriteLine("Исходный массив :hello , 12365 , cat , dog ,  5689 , 56");
+Console.WriteLine("Новый массив строк , длина которых меньше или равна 3 символа :");
 string[] arrayNew = new string[array.Length]; // новый иассив строк 
 int i ;
 int n=3;
@@ -12,6 +14,7 @@ for( i=0 ; i<array.Length ; i++)
     if ( array[i].Length<= n )
       {
         arrayNew[j]= array[i];
+        Console.WriteLine(arrayNew[j]);
       }
       else
         {
@@ -19,6 +22,6 @@ for( i=0 ; i<array.Length ; i++)
         }
 }
 
-Console.WriteLine("array[{0}] = {1}", i, array[i]);
+
 
     
